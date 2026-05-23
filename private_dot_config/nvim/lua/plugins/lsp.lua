7b48e9812df01c11f6dc03fa8351c21a133df8f7
aifrak <aifrak@users.noreply.github.com>
2026-05-23 14:47:20 +0200

    Extract into mason-tool-installer.lua

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -6,66 +6,6 @@ return {
     end,
   },
   {
-  "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      require("mason-tool-installer").setup({
-        -- Install Linters and Formatters and LSP without easy-install
-        ensure_installed = {
-          -- Lua
-          "stylua",
-          "selene",
-          -- sh, bash, zsh
-          "shfmt",
-          "shellcheck",
-          "shellharden",
-          -- Kubernetes
-          "kube-linter",
-          -- JSON
-          "jq",
-          "jq-lsp",
-          "jsonld-lsp",
-          -- YAML
-          "yamllint",
-          -- Helm, terraform
-          "hclfmt",
-          -- Dockerfile
-          "hadolint",
-          -- SQL
-          "pgformatter",
-          "sqlfluff",
-          -- dotenv
-          "dotenv-linter",
-          -- Editorconfig
-          "editorconfig-checker",
-          -- Git commits
-          "commitlint",
-          "gitlint",
-          -- HTML
-          "markuplint",
-          -- Makefile
-          "checkmake",
-          "mbake",
-          -- Markdown
-          "markdownlint-cli2",
-          "cbfmt",
-          "rumdl",
-          -- Spell check and Grammar check
-          "typos-lsp",
-          "vale",
-          "cspell",
-          -- GitHub Actions
-          "actionlint",
-          -- General
-          "prettierd",
-          -- snacks.nvim (image)
-          "mmdc", -- mermaid
-          "tectonic", -- LaTeX math expressions
-        },
-      })
-    end,
-  },
-  {
     "williamboman/mason-lspconfig.nvim",
     dependencies = { "williamboman/mason.nvim" },
     config = function()
