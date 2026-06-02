40ad2467274cd9610f78ecaf9150ce550ba3fb39
aifrak <aifrak@users.noreply.github.com>
2026-06-02 22:20:03 +0200

    Call mise before other commands

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -15,12 +15,12 @@ Set-Alias -Name lla -Value lsd_list_all -Option AllScope
 Function lsd_tree() { ls --tree }
 Set-Alias -Name lt -Value lsd_tree -Option AllScope
 
+(&mise activate pwsh) | Out-String | Invoke-Expression
+
 # fzf inside PowerShell
 Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 $commandOverride = [ScriptBlock]{ param($Location) Write-Host $Location }
 Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
-
-mise activate pwsh | Out-String | Invoke-Expression
 
 oh-my-posh init pwsh --config ~\custom-theme.omp.yml | Invoke-Expression
